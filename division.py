e13d50d7fb83dc28a1baef23df6db131b24ea057
Gus <149393@coderacademy.edu.au>
2023-12-01 17:37:25 +1100

    Added division feature

---
+++ division.py
@@ -0,0 +1,14 @@
+# user input nominator as n and denominator as d
+n = int(input("enter numerator: "))
+d = int(input("enter denominator: "))
+# if d == 0
+if d == 0:
+    print("denominator cannot be zero")
+    # display "denominator cannot be zero" as output
+# else
+else:
+    # q = n / d
+    q = n / d 
+    # display q
+    print(q)
+    
